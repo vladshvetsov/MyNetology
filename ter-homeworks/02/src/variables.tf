73124f85493b05976bde5fe8ba0f7e618907c87b
Vlad <vladshvetsov@yandex.ru>
2026-01-27 12:24:33 +0300

    First commit

---
+++ ter-homeworks/02/src/variables.tf
@@ -0,0 +1,40 @@
+###cloud vars
+
+
+variable "cloud_id" {
+  type        = string
+  default     = "b1gvibjotoc6e7f5j8a5"
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/cloud/get-id"
+}
+
+variable "folder_id" {
+  type        = string
+  default     = "b1gfr53gu9f2vbflut6n"
+  description = "https://cloud.yandex.ru/docs/resource-manager/operations/folder/get-id"
+}
+
+variable "default_zone" {
+  type        = string
+  default     = "ru-central1-a"
+  description = "https://cloud.yandex.ru/docs/overview/concepts/geo-scope"
+}
+variable "default_cidr" {
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+  description = "https://cloud.yandex.ru/docs/vpc/operations/subnet-create"
+}
+
+variable "vpc_name" {
+  type        = string
+  default     = "develop"
+  description = "VPC network & subnet name"
+}
+
+
+###ssh vars
+
+variable "vms_ssh_root_key" {
+  type        = string
+  default     = "/home/vlad/netology_github/MyNetology/ter-homeworks/terraform-cloud/your_ssh_ed25519_key.pub"
+  description = "ssh-keygen -t ed25519"
+}
